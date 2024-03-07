a4d920549629c9915a1a2e7d1e74ba38819d3cb8
hesham ahmed <heshamahmed191988@gmail.com>
2024-03-07 11:38:57 +0200

    authontication and authorization stage

---
+++ Jumia.Application/Services/AuthService.cs
@@ -0,0 +1,62 @@
+﻿using Jumia.Dtos.ViewModel;
+using Jumia.Model;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jumia.Application.Services
+{
+    public class AuthService:IAuthService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AuthService(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task<IdentityResult> RegisterUserAsync(RegisterViewModel model)
+        {
+
+            
+            var user = new ApplicationUser
+            {
+                UserName = model.UserName,
+                Email = model.Email,
+                PasswordHash=model.Password,
+                
+
+            };
+            
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded )
+            {
+                // Perform the sign-in
+                await _signInManager.SignInAsync(user, isPersistent: false);
+            }
+
+
+            return result;
+        }
+        public async Task<SignInResult> SignInAsync(LoginViewModel model)
+        {
+            var result = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe, lockoutOnFailure: false);
+            return result;
+        }
+        public async Task LogoutAsync()
+        {
+            await _signInManager.SignOutAsync();
+
+        }
+    }
+
+
+}
+
